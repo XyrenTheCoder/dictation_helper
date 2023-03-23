1643cd949eeed4f35697af37392866688e2280a6
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2023-03-23 09:54:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from colorama import Fore
+
+target = str(input("enter text: "))
+
+string = str(input("> "))
+
+out = []
+for i in string:
+    for a in target:
+       if i != a:
+           out.append(Fore.RED + i)
+       else:
+           out.append(i)
+
+print(f"text: {target}")
+print(f"your input: {''.join(out)}")
